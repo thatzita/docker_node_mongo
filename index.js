36c0954d5c821c2a38b808a5933e2f4c63bde275
thatzita <robinudehn@gmail.com>
2019-04-17 09:16:40 +0200

    added volume tweak

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require("express");
-
-const app = express();
-const port = 3000;
-
 const mongoose = require("mongoose");
-const DATABASE_CONNECTION = "mongodb://mongo:27017/database";
+const port = 3000;
+const DATABASE_CONNECTION = "mongodb://192.168.99.100:27017/test";
+const app = express();
+
+let Schema = mongoose.Schema;
+
+let pokemonSchema = new Schema(
+  {
+    name: String
+  },
+  { collection: "pokemon" }
+);
+
+let Pokemon = mongoose.model("Pokemon", pokemonSchema);
 
 mongoose
   .connect(DATABASE_CONNECTION, { useNewUrlParser: true })
   .then(() => {
-    console.log("Kopplad databasen");
+    console.log("Kopplad till databasen");
   })
   .catch(err => {
     throw err;
   });
 
 app.get("/", (req, res) => {
-  res.send("Working, we are up and running!");
+  res.send("Working, we are up and running! AGAIN! ;)");
+});
+
+app.get("/api/gottacatchemall", (req, res) => {
+  Pokemon.find().then(doc => {
+    console.log(doc);
+    res.send(doc);
+  });
 });
 
 app.listen(port, () => {
