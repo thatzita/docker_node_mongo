795491600783f2a76a86f02e1df72da8a2d6be4b
thatzita <robinudehn@gmail.com>
2019-05-31 08:23:32 +0200

    cleanup and refractoring code

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,274 +18,68 @@ const places_to_visit = type.category_places_to_visit;
 const User = require("../../models/User");
 const LocationInformation = require("../../models/LocationInformation");
 
-router.get("/getuser", (req, res) => {
-  let id = req.query.id;
-  let token = req.query.token;
+router.post("/getuser", (req, res) => {
+  let data = req.body.userData;
+  let token = req.body.token;
 
+  let user = {
+    id: data.id,
+    username: data.username,
+    profile_picture: data.profile_picture,
+    facebook_friends: [],
+    facebook_token: null,
+    full_name: data.full_name,
+    bio: data.bio,
+    website: data.website,
+    followers: data.counts.followed_by
+  };
+  res.send("added/updated in db");
+  updateDbWithUser(user, token);
+});
+
+router.get("/getuserdata", (req, res) => {
+  let id = req.query.token;
   User.findOne({ id: id }, (err, doc) => {
     if (err) res.send(err);
-    // Location.deleteMany({ user_id: id }, (err, doc) => {
-    //   if (err) console.log(err);
-    // });
-
-    getAllUserInfo(token); //send token and data from homescreen and settingscreen the object
+    res.send(doc);
   });
 });
 
-async function getAllUserInfo(token) {
-  let userInfo = null;
-  let userData = null;
-  let pagination = null;
+async function getAllInstagramPictures(token, userInfo) {
+  let pagination, userPictures, userId;
+  let user = userInfo;
+  let nextUrl;
 
   await axios
     .get(
       `https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}`
     )
     .then(res => {
-      userData = res.data.data;
+      userPictures = res.data.data;
       pagination = res.data.pagination;
+      nextUrl = pagination.next_url;
+      userId = res.data.data[0].user.id;
     })
     .catch(err => {
       console.log(err);
     });
 
-  if (userInfo && userData) {
-    let pinArr = [];
-    userData.forEach(info => {
-      if (info.location) {
-        LocationInformation.findOne(
-          {
-            name: info.location.name,
-            latitude: info.location.latitude,
-            longitude: info.location.longitude
-          },
-          (err, doc) => {
-            if (err) res.send(err);
-            if (doc) {
-              let image_id = info.images.standard_resolution.url;
-              let regex = /([^/]+$)/;
-              let docId = regex.exec(image_id);
-              let caption;
-              let category_type = ["other"];
-
-              doc.category_list.forEach(type => {
-                if (restaurant.includes(type.name)) {
-                  let pop = category_type.filter(x => x !== "other");
-                  category_type = pop;
-                  category_type.push("restaurant");
-                }
-                if (bar.includes(type.name)) {
-                  let pop = category_type.filter(x => x !== "other");
-                  category_type = pop;
-                  category_type.push("bar");
-                }
-                if (cafe.includes(type.name)) {
-                  let pop = category_type.filter(x => x !== "other");
-                  category_type = pop;
-                  category_type.push("cafe");
-                }
-                if (hotel.includes(type.name)) {
-                  let pop = category_type.filter(x => x !== "other");
-                  category_type = pop;
-                  category_type.push("hotel");
-                }
-                if (shopping.includes(type.name)) {
-                  let pop = category_type.filter(x => x !== "other");
-                  category_type = pop;
-                  category_type.push("shopping");
-                }
-              });
-
-              if (info.caption !== null) {
-                caption = info.caption.text;
-              } else {
-                caption = "";
-              }
-
-              if (info.carousel_media) {
-                let carouselArr = [];
-                info.carousel_media.forEach(image => {
-                  if (image.images) {
-                    carouselArr.push(image.images.standard_resolution.url);
-                  }
-                });
-
-                let pinInformation = {
-                  // geohash_id: pictureGeohash,
-                  followers: userInfo.counts.followed_by,
-                  user_id: userInfo.id,
-                  profile_picture: userInfo.profile_picture,
-                  category_type: category_type,
-                  username: userInfo.username,
-                  full_name: userInfo.full_name,
-                  carousel: carouselArr,
-                  image: info.images.standard_resolution.url,
-                  caption: caption,
-                  image_id: docId[0],
-                  location_info: {
-                    type: "Point",
-                    coordinates: [
-                      info.location.longitude,
-                      info.location.latitude
-                    ],
-                    latitude: info.location.latitude,
-                    longitude: info.location.longitude,
-                    name: info.location.name
-                  }
-                };
-                pinArr.push(pinInformation);
-              } else {
-                let pinInformation = {
-                  // geohash_id: pictureGeohash,
-                  followers: userInfo.counts.followed_by,
-                  user_id: userInfo.id,
-                  profile_picture: userInfo.profile_picture,
-                  category_type: category_type,
-                  username: userInfo.username,
-                  full_name: userInfo.full_name,
-                  image: info.images.standard_resolution.url,
-                  image_id: docId[0],
-                  caption: caption,
-                  location_info: {
-                    type: "Point",
-                    coordinates: [
-                      info.location.longitude,
-                      info.location.latitude
-                    ],
-                    latitude: info.location.latitude,
-                    longitude: info.location.longitude,
-                    name: info.location.name
-                  }
-                };
-                pinArr.push(pinInformation);
-              }
-
-              let user = {
-                id: userInfo.id,
-                username: userInfo.username,
-                profile_picture: userInfo.profile_picture,
-                facebook_friends: [],
-                facebook_token: null,
-                full_name: userInfo.full_name,
-                bio: userInfo.bio,
-                website: userInfo.website,
-                followers: userInfo.counts.followed_by,
-                geohash: pinArr //might not be necessary
-              };
-
-              if (pagination.next_url !== undefined) {
-                searchMorePictures(pagination.next_url, userInfo);
-              }
-              updateDbWithUser(user);
-              updatePictures(pinArr, userInfo.id);
-            } else {
-              console.log(
-                "could not find a fb place something went wrong in LocationInformation.findOne"
-              );
-
-              let image_id = info.images.standard_resolution.url;
-              let regex = /([^/]+$)/;
-              let docId = regex.exec(image_id);
-              let caption;
-              let category_type = ["other"]; // default type
-
-              if (info.caption !== null) {
-                caption = info.caption.text;
-              } else {
-                caption = "";
-              }
-
-              if (info.carousel_media) {
-                let carouselArr = [];
-                info.carousel_media.forEach(image => {
-                  if (image.images) {
-                    carouselArr.push(image.images.standard_resolution.url);
-                  }
-                });
-
-                let pinInformation = {
-                  // geohash_id: pictureGeohash,
-                  followers: userInfo.counts.followed_by,
-                  user_id: userInfo.id,
-                  profile_picture: userInfo.profile_picture,
-                  category_type: category_type,
-                  username: userInfo.username,
-                  full_name: userInfo.full_name,
-                  carousel: carouselArr,
-                  image: info.images.standard_resolution.url,
-                  caption: caption,
-                  image_id: docId[0],
-                  location_info: {
-                    type: "Point",
-                    coordinates: [
-                      info.location.longitude,
-                      info.location.latitude
-                    ],
-                    latitude: info.location.latitude,
-                    longitude: info.location.longitude,
-                    name: info.location.name
-                  }
-                };
-                pinArr.push(pinInformation);
-              } else {
-                let pinInformation = {
-                  // geohash_id: pictureGeohash,
-                  followers: userInfo.counts.followed_by,
-                  user_id: userInfo.id,
-                  profile_picture: userInfo.profile_picture,
-                  category_type: category_type,
-                  username: userInfo.username,
-                  full_name: userInfo.full_name,
-                  image: info.images.standard_resolution.url,
-                  image_id: docId[0],
-                  caption: caption,
-                  location_info: {
-                    type: "Point",
-                    coordinates: [
-                      info.location.longitude,
-                      info.location.latitude
-                    ],
-                    latitude: info.location.latitude,
-                    longitude: info.location.longitude,
-                    name: info.location.name
-                  }
-                };
-                pinArr.push(pinInformation);
-              }
-
-              // let user = {
-              //   id: userInfo.id,
-              //   username: userInfo.username,
-              //   profile_picture: userInfo.profile_picture,
-              //   facebook_friends: [],
-              //   facebook_token: null,
-              //   full_name: userInfo.full_name,
-              //   bio: userInfo.bio,
-              //   website: userInfo.website,
-              //   followers: userInfo.counts.followed_by,
-              // };
-
-              if (pagination.next_url !== undefined) {
-                searchMorePictures(pagination.next_url, userInfo);
-              }
-              //console.log('place ', pinArr)
-              updateDbWithUser(user);
-              updatePictures(pinArr, userInfo.id);
-              // console.log(info)
-            }
-          }
-        );
-      }
-    });
+  if (pagination.next_url) {
+    structurePictures(userPictures, user, userId);
+    searchMorePictures(nextUrl, user, userId);
+  } else {
+    structurePictures(userPictures, user, userId);
   }
 }
 
-async function searchMorePictures(url, userInfo) {
+async function searchMorePictures(nextUrl, userInfo, userId) {
   let userData = null;
   let pagination = null;
+  let rateLimit;
   await axios
-    .get(url)
+    .get(nextUrl)
     .then(res => {
+      rateLimit = res.headers["x-ratelimit-remaining"];
       userData = res.data.data;
       pagination = res.data.pagination;
     })
@@ -293,11 +87,109 @@ async function searchMorePictures(url, userInfo) {
       console.log(err);
     });
 
-  pins(userData, userInfo);
+  structurePictures(userData, userInfo, userId);
 
-  if (pagination.next_url !== undefined) {
-    searchMorePictures(pagination.next_url, userInfo);
+  if (pagination.next_url !== undefined && rateLimit > 50) {
+    searchMorePictures(pagination.next_url, userInfo, userId);
   }
+  console.log("Rate limit ", rateLimit);
+}
+
+function structurePictures(userPictures, user, userId) {
+  let pictureArr = [];
+  userPictures.forEach(info => {
+    if (info.location) {
+      LocationInformation.findOne(
+        {
+          name: info.location.name,
+          latitude: info.location.latitude,
+          longitude: info.location.longitude
+        },
+        (err, doc) => {
+          if (err) res.send(err);
+          if (doc) {
+            let image_id = info.images.standard_resolution.url;
+            let regex = /([^/]+$)/;
+            let docId = regex.exec(image_id);
+            let caption;
+            let carouselArr = [];
+
+            let category_type = categorizeImages(doc);
+
+            if (info.carousel_media) {
+              info.carousel_media.forEach(image => {
+                if (image.images) {
+                  carouselArr.push(image.images.standard_resolution.url);
+                }
+              });
+            } else {
+              carouselArr = [];
+            }
+
+            if (info.caption !== null) {
+              caption = info.caption.text;
+            } else {
+              caption = "";
+            }
+
+            let pinInformation = {
+              followers: user.followers,
+              user_id: user.id,
+              profile_picture: user.profile_picture,
+              category_type: category_type,
+              username: user.username,
+              full_name: user.full_name,
+              carousel: carouselArr,
+              image: info.images.standard_resolution.url,
+              caption: caption,
+              image_id: docId[0],
+              location_info: {
+                type: "Point",
+                coordinates: [info.location.longitude, info.location.latitude],
+                latitude: info.location.latitude,
+                longitude: info.location.longitude,
+                name: info.location.name
+              }
+            };
+            pictureArr.push(pinInformation);
+          }
+          updatePictures(pictureArr, userId);
+        }
+      );
+    }
+  });
+}
+
+function categorizeImages(doc) {
+  let category_type = ["other"];
+  doc.category_list.forEach(type => {
+    if (restaurant.includes(type.name)) {
+      let pop = category_type.filter(x => x !== "other");
+      category_type = pop;
+      category_type.push("restaurant");
+    }
+    if (bar.includes(type.name)) {
+      let pop = category_type.filter(x => x !== "other");
+      category_type = pop;
+      category_type.push("bar");
+    }
+    if (cafe.includes(type.name)) {
+      let pop = category_type.filter(x => x !== "other");
+      category_type = pop;
+      category_type.push("cafe");
+    }
+    if (hotel.includes(type.name)) {
+      let pop = category_type.filter(x => x !== "other");
+      category_type = pop;
+      category_type.push("hotel");
+    }
+    if (shopping.includes(type.name)) {
+      let pop = category_type.filter(x => x !== "other");
+      category_type = pop;
+      category_type.push("shopping");
+    }
+  });
+  return category_type;
 }
 
 async function updatePictures(arr, id) {
@@ -320,7 +212,7 @@ async function updatePictures(arr, id) {
   });
 }
 
-function updateDbWithUser(user) {
+function updateDbWithUser(user, token) {
   let options = {
     upsert: true,
     returnOriginal: false
@@ -329,129 +221,6 @@ function updateDbWithUser(user) {
     if (err) console.log(err);
     return doc;
   });
+  getAllInstagramPictures(token, user);
 }
-
-function pins(userData, userInfo) {
-  let pinArr = [];
-  userData.forEach(info => {
-    if (info.location) {
-      LocationInformation.findOne(
-        {
-          name: info.location.name,
-          latitude: info.location.latitude,
-          longitude: info.location.longitude
-        },
-        (err, doc) => {
-          if (err) res.send(err);
-          if (doc) {
-            let image_id = info.images.standard_resolution.url;
-            let regex = /([^/]+$)/;
-            let docId = regex.exec(image_id);
-            let caption;
-            let category_type = ["other"]; // default type
-
-            doc.category_list.forEach(type => {
-              if (restaurant.includes(type.name)) {
-                let pop = category_type.filter(x => x !== "other");
-                category_type = pop;
-                category_type.push("restaurant");
-              }
-              if (bar.includes(type.name)) {
-                let pop = category_type.filter(x => x !== "other");
-                category_type = pop;
-                category_type.push("bar");
-              }
-              if (cafe.includes(type.name)) {
-                let pop = category_type.filter(x => x !== "other");
-                category_type = pop;
-                category_type.push("cafe");
-              }
-              if (hotel.includes(type.name)) {
-                let pop = category_type.filter(x => x !== "other");
-                category_type = pop;
-                category_type.push("hotel");
-              }
-              if (shopping.includes(type.name)) {
-                let pop = category_type.filter(x => x !== "other");
-                category_type = pop;
-                category_type.push("shopping");
-              }
-            });
-
-            if (info.caption !== null) {
-              caption = info.caption.text;
-            } else {
-              caption = "";
-            }
-
-            if (info.carousel_media) {
-              let carouselArr = [];
-              info.carousel_media.forEach(image => {
-                if (image.images) {
-                  carouselArr.push(image.images.standard_resolution.url);
-                }
-              });
-
-              let pinInformation = {
-                // geohash_id: pictureGeohash,
-                followers: userInfo.counts.followed_by,
-                user_id: userInfo.id,
-                profile_picture: userInfo.profile_picture,
-                category_type: category_type,
-                username: userInfo.username,
-                full_name: userInfo.full_name,
-                carousel: carouselArr,
-                image: info.images.standard_resolution.url,
-                caption: caption,
-                image_id: docId[0],
-                location_info: {
-                  type: "Point",
-                  coordinates: [
-                    info.location.longitude,
-                    info.location.latitude
-                  ],
-                  latitude: info.location.latitude,
-                  longitude: info.location.longitude,
-                  name: info.location.name
-                }
-              };
-              pinArr.push(pinInformation);
-            } else {
-              let pinInformation = {
-                // geohash_id: pictureGeohash,
-                followers: userInfo.counts.followed_by,
-                user_id: userInfo.id,
-                profile_picture: userInfo.profile_picture,
-                category_type: category_type,
-                username: userInfo.username,
-                full_name: userInfo.full_name,
-                image: info.images.standard_resolution.url,
-                image_id: docId[0],
-                caption: caption,
-                location_info: {
-                  type: "Point",
-                  coordinates: [
-                    info.location.longitude,
-                    info.location.latitude
-                  ],
-                  latitude: info.location.latitude,
-                  longitude: info.location.longitude,
-                  name: info.location.name
-                }
-              };
-              pinArr.push(pinInformation);
-            }
-
-            updatePictures(pinArr, userInfo.id);
-          } else {
-            console.log(
-              "could not find a fb place something went wrong in LocationInformation.findOne"
-            );
-          }
-        }
-      );
-    }
-  });
-}
-
 module.exports = router;
